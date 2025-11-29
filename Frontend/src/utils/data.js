9dca078e4866ae41017027fea18c2d2475035fcb
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2025-11-29 11:55:18 +0530

    Update data.js

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "https://hirehub-backend-2.onrender.com/api/user";
-export const JOB_API_ENDPOINT = "https://hirehub-backend-2.onrender.com/api/job";
-export const APPLICATION_API_ENDPOINT = "https://hirehub-backend-2.onrender.com/api/application";
-export const COMPANY_API_ENDPOINT = "https://hirehub-backend-2.onrender.com/api/company";
+export const USER_API_ENDPOINT = "https://hirehub-updated-backend.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://hirehub-updated-backend.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://hirehub-updated-backend.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://hirehub-updated-backend.onrender.com/api/company";
